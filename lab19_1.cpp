b6685aabadeca9742bd45abe2ba6089585560ddf
Yamami1221 <anak2547@gmail.com>
2023-02-17 16:43:12 +0700

    lab 19_1.cpp completed

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,57 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream source;
+    source.open(filename);
+    string textline;
+    char format[] = "%[^:]: %d %d %d";
+    string name;
+    char cname[100];
+    int score1, score2, score3, scoreSum;
+    char grade;
+    while (getline(source,textline)){
+        sscanf(textline.c_str(),format,cname,&score1,&score2,&score3);
+        name = cname;
+        scoreSum = score1 + score2 + score3;
+        grade = score2grade(scoreSum);
+        names.push_back(name);
+        scores.push_back(scoreSum);
+        grades.push_back(grade);
+    }
+    source.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command: ";
+    getline(cin,command);
+    int i = command.find_first_of(" ");
+    key = command.substr(i+1);
+    command = command.substr(0,i);
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    cout << "---------------------------------\n";
+    for(unsigned i = 0; i < names.size(); i++){
+        if(toUpperStr(names[i]) == key){
+            cout << names[i] << "'s score = " << scores[i] << endl;
+            cout << names[i] << "'s grade = " << grades[i] << endl;
+            cout << "---------------------------------\n";
+            return;
+        }
+    }
+    cout << "Cannot found.\n";
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    cout << "---------------------------------\n";
+    for(unsigned i = 0; i < names.size(); i++){
+        if(grades[i] == key[0]){
+            cout << names[i] << " (" << scores[i] << ")" << endl;
+        }
+    }
+    cout << "---------------------------------\n";
 }
 
 
